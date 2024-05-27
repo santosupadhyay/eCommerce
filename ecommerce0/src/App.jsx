9401e8fff108fbec3ea7e50205bc5e198db2e44b
santosupadhyay <itsmr.santoshupadhyay18@gmail.com>
2024-05-27 15:11:44 +0545

    App updated

--- ecommerce0/src/App.jsx
+++ ecommerce0/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
 import './App.css'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import Navbar from './components/Navbar'
+import Search from './components/Search'
+import ProductDetail from './components/ProductDetail'
+
 
 function App() {
   // const [count,setCount] = useState(0)
 
   return (
     <>
-      Hello Achyut Mishra
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={ <Cart />} />
+        <Route path='/search' element={ <Search />} />
+        <Route path='/productdetail' element={<ProductDetail />} />
+      </Routes>
+    </Router>
+                   
     </>
   )
 }
